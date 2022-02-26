e8694d1d5e638bbec35c43b9e41773f82303895f
dotoracle <dotoracle.network@gmail.com>
2022-02-26 14:42:05 +0700

    shorten mintid key through blake

--- erc20/src/mintids.rs
+++ erc20/src/mintids.rs
@@ -2,7 +2,7 @@
 use alloc::{string::String};
 
 use casper_contract::{
-    contract_api::{storage},
+    contract_api::{storage, runtime}, 
     unwrap_or_revert::UnwrapOrRevert,
 };
 use casper_types::{URef};
@@ -14,17 +14,25 @@ pub(crate) fn mintids_uref() -> URef {
     detail::get_uref("mintids")
 }
 
+fn make_dictionary_item_key(mintid: &String) -> String {
+    let preimage = mintid.as_bytes();
+    let key_bytes = runtime::blake2b(&preimage);
+    hex::encode(&key_bytes)
+}
+
 /// Writes an allowance for owner and spender for a specific amount.
 pub(crate) fn write_mintid_to(
     mintids_uref: URef,
     mintid: &String
 ) {
-    storage::dictionary_put(mintids_uref, mintid, 1)
+    let dictionary_item_key = make_dictionary_item_key(mintid);
+    storage::dictionary_put(mintids_uref, &dictionary_item_key, 1)
 }
 
 /// Reads an allowance for a owner and spender
 pub(crate) fn read_mintid_from(mintids_uref: URef, mintid: &String) -> u64 {
-    storage::dictionary_get(mintids_uref, mintid)
+    let dictionary_item_key = make_dictionary_item_key(mintid);
+    storage::dictionary_get(mintids_uref, &dictionary_item_key)
         .unwrap_or_revert()
         .unwrap_or_default()
 }
